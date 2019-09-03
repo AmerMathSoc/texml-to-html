394cadb6397275b17aaf315095556603af8dab26
Peter Krautzberger <p.krautzberger@gmail.com>
2019-09-03 09:44:43 +0200

    chore: remove console.log

--- test/ref-list-ref-label.js
+++ test/ref-list-ref-label.js
@@ -7,7 +7,6 @@ tape('ref-list, ref-list/title, ref, ref/label', async function(t) {
   t.plan(4);
   const input = path.resolve(__dirname, 'ref-list-ref-label.xml');
   const document = await xsltproc(input);
-  console.log(document.body.outerHTML);
   const bibliography = document.querySelector('section[role="doc-bibliography"]');
   t.ok(bibliography, 'Section with role doc-bibliography');
   t.ok(bibliography.querySelector('h2'), 'Bibliography heading in article');
