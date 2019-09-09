673971ffc787950e82b26b81c99a430521b85bc7
Peter Krautzberger <p.krautzberger@gmail.com>
2019-09-09 15:06:49 +0200

    chore: remove console.log

--- test/element-email.js
+++ test/element-email.js
@@ -9,7 +9,6 @@ tape('Template: email', async function(t) {
     'element-email.xml'
   );
   const document = await xsltproc(input);
-  console.log(document.body.innerHTML);
   const email = document.querySelector('a[href="mailto://address1"]');
   t.equal(email.outerHTML, '<a href="mailto://address1">address1</a>', 'email to anchor with mailto address');
   t.equal(email.nextSibling.textContent, ', ', 'multiple email elements are separated by comma');
