aae02c3264f4c195f27d6030279261d61cb703a7
Peter Krautzberger <p.krautzberger@gmail.com>
2019-10-30 16:31:38 +0100

    chore: clean up console.log

--- test/element-stripEmptyLabel.js
+++ test/element-stripEmptyLabel.js
@@ -6,7 +6,6 @@ tape('Empty Labels should be stripped', async function(t) {
   t.plan(6);
   const input = path.resolve(__dirname, 'element-stripEmptyLabel.xml');
   const document = await xsltproc(input);
-  console.log(document.body.innerHTML)
   t.equal(document.querySelector('nav[role="doc-toc"] ol li a').innerHTML, '', 'Toc-entry with (empty) title and empty label');
   t.equal(document.querySelector('section[data-ams-doc-level="1"] h2').innerHTML, '', 'Sec with (empty) title and empty label');
   t.equal(document.querySelector('section[data-ams-doc="statement"]').innerHTML.trim(), '<span data-ams-doc="secheading"></span>', 'Statement with (empty) title and empty label');
