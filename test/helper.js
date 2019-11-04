94136b25a68c270a70a3f0f2f7e11ef9191a6ce3
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-04 11:54:14 +0100

    switch helpers to main.js

--- test/helper.js
+++ test/helper.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const xslt = path.resolve(__dirname, '../ams-xml-to-html.xsl');
 const jsdom = require("jsdom");
@@ -6,8 +7,18 @@ const exec = require('child_process').exec;
 const {promisify} = require('util');
 const execAsync = promisify(exec);
 
+const xml2html = require('../main.js');
+
 exports.xsltproc = async input => {
   const output = await execAsync(`xsltproc ${xslt} ${input}`);
   const { window } = new JSDOM(output.stdout);
   return window.document;
+};
+
+exports.xsltproc = async input => {
+  const inputString = fs.readFileSync(input);
+  console.log(xml2html);
+  const dom = xml2html(inputString);
+  console.log(dom.serialize());
+  return dom.window.document;
 };
