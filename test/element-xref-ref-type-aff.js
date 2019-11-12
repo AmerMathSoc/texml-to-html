e37531666263d32ec4682f40220563d3ecb2e107
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-12 21:17:11 +0100

    feat:pass element-xref-ref-type-aff.js
    Switches to element-article-meta.xml and deletes old xml.
    Fixes test to pass in JS and XSL.

--- test/element-xref-ref-type-aff.js
+++ test/element-xref-ref-type-aff.js
@@ -6,10 +6,10 @@ tape('Template: contrib-group/xref@ref-type="aff"', async function(t) {
   t.plan(2);
   const input = path.resolve(
     __dirname,
-    'element-xref-ref-type-aff.xml'
+    'element-article-meta.xml'
   );
   const document = await xsltproc(input);
-  const link1 = document.querySelector('section[data-ams-doc="copyright-page"] dl dl dd');
+  const link1 = document.querySelector('dt[data-ams-doc-contrib="contribB name"]').nextElementSibling;
   t.equal(link1.innerHTML.trim(), '<span>Address at time of publication: </span>Aff1', 'xref ref-type aff linking to aff with specific-use=current creates DD with content');
   t.equal(link1.nextElementSibling.outerHTML.trim(), '<dd>Aff2</dd>', 'xref ref-type aff creates DD with content');
 });
