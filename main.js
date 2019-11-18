5989d36200aad83b94af0679efd3c9c4d5858586
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-18 13:06:07 +0100

    fix: abstact()
    Adapt to changes in level handling due to sec(), label()

--- main.js
+++ main.js
@@ -720,7 +720,7 @@ const elementProcessor = {
     passThrough(xmldoc, htmldoc, div, xmlnode);
   },
   abstract: (xmldoc, htmldoc, htmlParentNode, xmlnode) => {
-    const level = getParentLevel(htmlParentNode) || '2'; // NOTE in articles, we don't have a disp-level in the XML; also NOTE that this is a change from xslt which erroneously had hardcoded 1 but abstract/title still got an h2
+    const level = getParentLevel(htmlParentNode) || '1'; // NOTE in articles, we don't have a disp-level in the XML; also NOTE that this is a change from xslt which erroneously had hardcoded 1 but abstract/title still got an h2
     const section = createNode(htmldoc, 'section', '', {
       'data-ams-doc-level': level,
       role: 'doc-abstract'
