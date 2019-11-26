330b5da38d9b9d3742d7290e621646fc39fb2b2c
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-26 12:34:24 +0100

    feat: pass attribute-content-type.js
    Switch test to article.xml and remove old xml.

--- test/attribute-content-type.js
+++ test/attribute-content-type.js
@@ -5,7 +5,7 @@ const tape = require('tape');
 
 tape('Template: @content-type', async function(t) {
   t.plan(1);
-  const input = path.resolve(__dirname, 'attribute-content-type.xml');
+  const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
   const style = document.querySelector('[data-ams-content-type]');
   t.ok(style, 'Element with data-ams-content-type attribute');
