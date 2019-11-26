324679e6185bcc98ca581fcb568f835cbc944a77
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-26 12:40:51 +0100

    feat: pass attribute-hidden.js
    Switch test to article.xml and move test case there.

--- test/attribute-hidden.js
+++ test/attribute-hidden.js
@@ -5,7 +5,7 @@ const tape = require('tape');
 
 tape('Template: @hidden', async function(t) {
   t.plan(1);
-  const input = path.resolve(__dirname, 'attribute-hidden.xml');
+  const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
   const hiddenEl = document.querySelector('[hidden]');
   t.ok(hiddenEl, 'Element with hidden attribute');
