b3d2f5e8a39149e9ab4a4b92a2027805b204ad3a
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-26 16:34:09 +0100

    feat: pass element-book-preface.js
    Switch test to book.xml and remove old xml.

--- test/element-book-preface.js
+++ test/element-book-preface.js
@@ -2,10 +2,10 @@ const path = require('path');
 const xsltproc = require('./helper.js').xsltproc;
 const tape = require('tape');
 
-tape('Template: preface ', async function(t) {
+tape('Template: book preface ', async function(t) {
   t.plan(1);
 
-  const input = path.resolve(__dirname, 'element-book-preface.xml');
+  const input = path.resolve(__dirname, 'book.xml');
   const document = await xsltproc(input);
   const preface = document.querySelector('section[role="doc-preface"]');
   t.ok(preface, 'preface as section with role doc-prefaces');
