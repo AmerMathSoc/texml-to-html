23829c3d00971190a67d7aba686ebffa20555e3a
Peter Krautzberger <p.krautzberger@gmail.com>
2019-11-26 12:40:23 +0100

    feat: pass attribute-has-qed-box.js
    Switch to article-xml and remove old xml.

--- test/attribute-has-qed-box.js
+++ test/attribute-has-qed-box.js
@@ -5,7 +5,7 @@ const tape = require('tape');
 
 tape('Template: @position', async function(t) {
   t.plan(1);
-  const input = path.resolve(__dirname, 'attribute-has-qed-box.xml');
+  const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
   const qedboxEl = document.querySelector('[data-ams-qed-box]');
   t.ok(qedboxEl, 'Element with data-ams-qed-box attribute');
