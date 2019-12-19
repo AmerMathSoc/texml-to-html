affb6d29d187ea0cf5a27090ae36b326e6f8c87f
Peter Krautzberger <p.krautzberger@gmail.com>
2019-12-19 10:32:22 +0100

    fix: preface data-ams-doc-level
    Sets doc-level of 0 (like chapter),
    ensures h1 for title (instead of "hnan").
    Updates tests.

--- test/element-book-preface.js
+++ test/element-book-preface.js
@@ -3,10 +3,11 @@ const xsltproc = require('./helper.js').xsltproc;
 const tape = require('tape');
 
 tape('Template: book preface ', async function(t) {
-  t.plan(1);
+  t.plan(2);
 
   const input = path.resolve(__dirname, 'book.xml');
   const document = await xsltproc(input);
   const preface = document.querySelector('section[role="doc-preface"]');
   t.ok(preface, 'preface as section with role doc-prefaces');
+  t.ok(preface.querySelector('h1'), 'Preface title should become an h1');
 });
