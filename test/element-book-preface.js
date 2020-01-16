0b60924831940d6bc6bda53ed6dbc2c88d32f61b
Peter Krautzberger <p.krautzberger@gmail.com>
2020-01-16 12:22:59 +0100

    fix(preface.js): preserve attributes
    Adds mapAttributes.
    Adds test.
    
    Fixes #258

--- test/element-book-preface.js
+++ test/element-book-preface.js
@@ -2,12 +2,13 @@ const path = require('path');
 const xsltproc = require('./helper.js').xsltproc;
 const tape = require('tape');
 
-tape('Template: book preface ', async function(t) {
-  t.plan(2);
+tape('Template: book preface', async function(t) {
+  t.plan(3);
 
   const input = path.resolve(__dirname, 'book.xml');
   const document = await xsltproc(input);
   const preface = document.querySelector('section[role="doc-preface"]');
   t.ok(preface, 'preface as section with role doc-preface');
+  t.equal(preface.id, 'preface', 'preface preserves ID');
   t.ok(preface.querySelector('h1'), 'Preface title should become an h1');
 });
