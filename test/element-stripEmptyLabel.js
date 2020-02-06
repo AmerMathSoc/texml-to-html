b1c4fbcea878116f2213999489b0a92e203124b9
Peter Krautzberger <p.krautzberger@gmail.com>
2020-02-06 09:22:43 +0100

    fix(label.js): call trim correctly
    Correctly detects empty label so that title+empty label works correctly.
    Also adds tests.
    
    Fixes #273

--- test/element-stripEmptyLabel.js
+++ test/element-stripEmptyLabel.js
@@ -3,8 +3,9 @@ const xsltproc = require('./helper.js').xsltproc;
 const tape = require('tape');
 
 tape('Empty Labels should be stripped', async function(t) {
-  t.plan(1);
+  t.plan(2);
   const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
   t.equal(document.querySelector('#emptyLabel').innerHTML.trim(), '', 'Statement with no title and empty label');
+  t.equal(document.querySelector('#titleEmptyLabel').innerHTML.trim(), '<h3>Title. </h3>', 'Statement with no title and empty label');
 });
