baaf62ff8871df1c74ee6124baf7f37a724ab14f
Peter Krautzberger <p.krautzberger@gmail.com>
2020-03-24 15:41:02 +0100

    feat(label.js): preserve alt-title in books
    Stores the alt-title's content in data-ams-doc-alttitle.
    * label.js:
      * detect alt-title and rework subtitle handling
      * for books, store alt-title in data attribute for downstream use
    * add test cases.
    
    Resolves #287

--- test/element-sec-app-front-matter-part-dedication-title-label.js
+++ test/element-sec-app-front-matter-part-dedication-title-label.js
@@ -4,7 +4,7 @@ const tape = require('tape');
 
 
 tape('sec, app, front-matter-part, dedication, title, label', async function(t) {
-  t.plan(37);
+  t.plan(39);
   const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
 
@@ -36,6 +36,7 @@ tape('sec, app, front-matter-part, dedication, title, label', async function(t) 
   t.ok(document.querySelector('#apptitle header p[data-ams-doc="subtitle"]'), 'sec with title: subtitle to p with data-ams-doc');
   t.ok(document.querySelector('#applabel header p[data-ams-doc="subtitle"]'), 'sec with title: subtitle to p with data-ams-doc');
 
+  t.notOk(document.querySelector('#alttitle h2').hasAttribute('data-ams-doc-alttitle'), 'sec with title: subtitle to p with data-ams-doc');
 
   const input2 = path.resolve(__dirname, 'book.xml');
   const document2 = await xsltproc(input2);
@@ -63,6 +64,8 @@ tape('sec, app, front-matter-part, dedication, title, label', async function(t) 
   t.ok(document2.querySelector('#fmtitle header p[data-ams-doc="subtitle"]'), 'sec with title: subtitle to p with data-ams-doc');
   t.ok(document2.querySelector('#fmlabel header p[data-ams-doc="subtitle"]'), 'sec with title: subtitle to p with data-ams-doc');
   t.ok(document2.querySelector('#ack1[role="doc-acknowledgments"] h1'), 'ack to role doc-acknowledgments with data-ams-doc-level'); // NOTE should be testing doc-level but they differ in XSL vs JS
+
+  t.equal(document2.querySelector('#alttitle h1').getAttribute('data-ams-doc-alttitle'), 'Alt title', 'sec with title: subtitle to p with data-ams-doc');
 
   const input3 = path.resolve(__dirname, 'article--alttitle.xml');
   const document3 = await xsltproc(input3);
