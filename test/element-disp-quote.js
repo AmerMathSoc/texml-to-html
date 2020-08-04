2152d70c65ef1377229116e028056560980ccbf2
Peter Krautzberger <p.krautzberger@gmail.com>
2020-08-04 09:57:11 +0200

    feat(disp-quote): add role for epigraphs
    Adds ARIA role for epigraphs.
    Also adds test case and updates test.
    
    Resolves #245

--- test/element-disp-quote.js
+++ test/element-disp-quote.js
@@ -4,13 +4,13 @@ const tape = require('tape');
 
 
 tape('Template: disp-quote, disp-quote/attrib', async function(t) {
-  t.plan(2);
+  t.plan(4);
   const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
   const quotes = document.querySelectorAll('blockquote[data-ams-style="use"]');
   t.ok(quotes[0], 'disp-quote with specific-use to blockquote with data-ams-style');
   t.ok(quotes[0].querySelector('footer span'), 'attrib in disp-quote as footer with span');
-  // TODO XSLT cannot pass this yet. Activate after switch
-  // t.notEqual(quotes[1].parentNode.tagName, 'P', 'blockquote moved out of paragraph to avoid invalid HTML');
+  t.notEqual(quotes[1].parentNode.tagName, 'P', 'blockquote moved out of paragraph to avoid invalid HTML');
+  t.ok(document.querySelector('blockquote[role="doc-epigraph"]'), 'Epigraphs get role');
 });
 
