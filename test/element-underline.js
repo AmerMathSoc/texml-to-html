944f76d2618309c80fd831e6980746bda03f9794
Peter Krautzberger <p.krautzberger@gmail.com>
2020-11-10 11:07:46 +0100

    feat: revise underline handling
    Switches underline output from span@data-style  to u tags.
    * adds underline.js
    * recurseTheDom: switch to underline.js
    * update test
    
    Resolves #223

--- test/element-underline.js
+++ test/element-underline.js
@@ -7,6 +7,6 @@ tape('Template: underline', async function(t) {
   t.plan(1);
   const input = path.resolve(__dirname, 'article.xml');
   const document = await xsltproc(input);
-  t.ok(document.querySelector('span[data-ams-style="underline"]'), 'Underline as span with data-ams-style attribute');
+  t.ok(document.querySelector('u'), 'Underline u element');
 });
 
