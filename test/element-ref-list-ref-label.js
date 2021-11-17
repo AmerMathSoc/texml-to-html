fa6f4b9c5d5d7a6dee939dfd528d1fca11ab2e83
Peter Krautzberger <p.krautzberger@gmail.com>
2021-11-17 11:24:09 +0100

    feat(ref-list.js): support nested ref-list
    Adds logic to handle nested ref-lists.
    * recurseTheDom: adjust to change in ref-list() signature
    * ref-list.js
      * adjust signature to accept passThrough()
      * improve book check
      * add checks for nested reflists
      * only add role for unnested reflsit
      * passThrough on nested reflist
    * also adds test  case and test
    
    Resolves #331

--- test/element-ref-list-ref-label.js
+++ test/element-ref-list-ref-label.js
@@ -1,9 +1,9 @@
-const { article } = require('./helper.js');
+const { article, articleAlttitle } = require('./helper.js');
 const tape = require('tape');
 
 
-tape('ref-list, ref-list/title, ref, ref/label', async function(t) {
-  t.plan(5);
+tape('ref-list, ref-list/title, ref, ref/label', async function (t) {
+  t.plan(10);
   const document = article;
   const bibliography = document.querySelector('section[role="doc-bibliography"]');
   t.ok(bibliography, 'Section with role doc-bibliography');
@@ -12,4 +12,13 @@ tape('ref-list, ref-list/title, ref, ref/label', async function(t) {
   t.ok(bibliography.querySelector('dl dt#ref span'), 'Reference label as span');
 
   t.ok(bibliography.querySelector('dl dt#refnolabel+dd'), 'Reference without label');
- });
+
+  const document2 = articleAlttitle;
+  const bib = document2.querySelector('section[role="doc-bibliography"]');
+  t.ok(bib, 'Outer ref-list Section with role doc-bibliography');
+  t.ok(bib.querySelector('h2'), 'Outer ref-list heading');
+  t.notOk([...bib.children].filter(node => node.tagName === "DL").length, 'Outer ref-list does not have a DL child');
+  const nestedBib = bib.querySelector('section');
+  t.ok(nestedBib.querySelector('h3'), 'Inner ref-list heading');
+  t.ok(nestedBib.querySelector('dl'), 'Inner ref-list has DL');
+});
