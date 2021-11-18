8cb0656cf83ced2ccb994823086f6bed3febd9b5
Peter Krautzberger <p.krautzberger@gmail.com>
2021-11-18 12:23:28 +0100

    feat(styled-content): support color attributes
    Adds support for new color-related attributes due to texml's xcolor
    support.
    * styled-content
     * refactor style-type support
     * map text-, background-, and border color to data-ams-style-color
       (as a combined CSS declaration).
    * add test case and test
    
    Resolves #333

--- test/element-styled-content.js
+++ test/element-styled-content.js
@@ -4,8 +4,9 @@ const tape = require('tape');
 
 
 tape('Template: styled-content', async function(t) {
-  t.plan(1);
+  t.plan(2);
   const document = article;
   t.ok(document.querySelector('span[data-ams-style="type"]'), 'Styled-content as span with data-ams-style');
+  t.equal(document.querySelector('span[data-ams-style-color]').getAttribute('data-ams-style-color'), 'color:Teal;background-color:rgb(0.000%, 0.000%, 1.000%);border-color:rgb(0, 140, 10);', 'Styled-content as span with data-ams-style');
 });
 
