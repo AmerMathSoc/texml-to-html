13489f76aa9cc045995a6d039302d4b583408142
Peter Krautzberger <p.krautzberger@gmail.com>
2021-12-10 14:42:51 +0100

    feat: handle xref and ext-link inside xref
    Adds support to "unwrap" (some) nested links, namely
    xref which includes xref or ext-link elements.
    * recurseTheDom: pass recurseTheDom to xref()
    * xref.js
      * adjust signature to accept recurseTheDom
      * check for nested xref and ext-links
        * create span to catch all children from that point on.
    * add test case and test
    
    Resolves #332

--- test/element-xref.js
+++ test/element-xref.js
@@ -4,10 +4,12 @@ const tape = require('tape');
 
 
 tape('Template: xref', async function(t) {
-  t.plan(4);
+  t.plan(6);
   const document = article;
   t.ok(document.querySelector('a[href="#rid1"][data-ams-ref="type"]'), 'xref as anchor with href, data-ams-ref');
   t.ok(document.querySelector('a[href="#rid2"][data-ams-ref="fn"][role="doc-noteref"]'), 'xref with ref-type fn has role doc-noteref');
   t.ok(document.querySelector('cite a[href="#rid3"][data-ams-ref="bibr"][role="doc-biblioref"]'), 'xref with ref-type bibr a cite with anchor with href, data-ams-ref, role doc-biblioref');
   t.ok(document.querySelector('span[data-ams-ref="notrid"]'), 'xref without rid as span with data-ams-ref notrid');
+  t.notOk(document.querySelector('a[data-ams-ref="nested"] a'), 'xref with ext-link inside does not have nested link');
+  t.ok(document.querySelector('a[data-ams-ref="nested"]+span').querySelector('a[href="https://nested"]'), 'ext-link inside xref moved after xref');
 });
