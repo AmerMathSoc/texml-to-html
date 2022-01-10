04b194756dd05098332d378b1311fc1f01441831
Peter Krautzberger <p.krautzberger@gmail.com>
2022-01-10 10:46:05 +0100

    feat(fig): improve attribute preservation
    * Integrate mapAttributes()
    * only preserve position when present.
    * Add test case and test
    
    Resolves #339

--- test/element-fig-subfig-label.js
+++ test/element-fig-subfig-label.js
@@ -4,7 +4,7 @@ const tape = require('tape');
 
 
 tape('Template: fig, fig-group, caption, label', async function(t) {
-  t.plan(9);
+  t.plan(11);
   const document = article;
   const figurePosition = document.querySelector('#figures figure#position');
   t.equal(figurePosition.getAttribute('data-ams-position'), 'anchor', 'Figure with data-ams-position attribute');
@@ -17,4 +17,6 @@ tape('Template: fig, fig-group, caption, label', async function(t) {
   t.equal(labels[4].innerHTML, 'Grouplabel 2. ', 'Fig-group label without caption gets period');
   t.equal(labels[5].innerHTML, '(Sublabel 2) ', 'Subfigure label without caption gets parentheses');
   t.notOk(figurePosition.nextElementSibling.hasAttribute('id'), 'Fig without id does not create a (bad) id');
+  t.notOk(figurePosition.nextElementSibling.getAttribute('data-ams-position'), 'Fig without position does not create a (bad) data attribute');
+  t.equal(figurePosition.nextElementSibling.getAttribute('data-ams-specific-use'), 'special', 'Fig with specific-use');
 });
