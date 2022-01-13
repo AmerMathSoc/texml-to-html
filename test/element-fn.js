6b956f5b4b426a886a39996eb1182713da1e387a
Peter Krautzberger <p.krautzberger@gmail.com>
2022-01-13 11:51:31 +0100

    feat!(label.js): preserve label in fn
    Maps label to span instead of dropping it.
    * label.js: for label in fn, create span with data-ams-doc=label
    * add tests
    
    Resolves #342
    BREAKING CHANGE: footnote no longer drops its label.

--- test/element-fn.js
+++ test/element-fn.js
@@ -4,11 +4,14 @@ const tape = require('tape');
 
 
 tape('Template: fn, fn/label', async function(t) {
-  t.plan(3);
+  t.plan(5);
   const document = article;
   const footnote = document.querySelector('div[role="doc-footnote"]');
   t.ok(footnote, 'fn becomes div with role doc-footnote');
-  t.equal(footnote.innerHTML, '', 'footnote label is stripped');
+  const footnoteLabel = footnote.firstElementChild;
+  t.equal(footnoteLabel.tagName, 'SPAN', 'footnote label');
+  t.equal(footnoteLabel.getAttribute('data-ams-doc'), 'label', 'footnote label data attribute');
+  t.equal(footnoteLabel.innerHTML, 'Label', 'footnote label content');
   t.notOk(document.querySelector('#fnid').closest('p'), 'footnote moved out of paragraph')
 });
 
