14f742653939da6628552b72f259ba62898bd38b
Peter Krautzberger <p.krautzberger@gmail.com>
2022-01-20 14:48:36 +0100

    fix: unnest links within ext-link
    Avoids nested links from ext-link elements.
    * add unnest.js: extract unnesting logic  from xref.js
    * xref.js: refactor to use unnest.js
    * ext-link: integrate unnestLinks()
    * recurseTheDom: adjust to ext-link signature change
    * add test case and test
    
    Fixes #347

--- test/element-ext-link.js
+++ test/element-ext-link.js
@@ -3,10 +3,13 @@ const { article } = require('./helper.js');
 const tape = require('tape');
 
 
-tape('Template: ext-link', async function(t) {
-  t.plan(1);
+tape('Template: ext-link', async function (t) {
+  t.plan(3);
   const document = article;
+  console.log(document.querySelector('a[href="https://nested2"]').parentNode.outerHTML);
   const extlink = document.querySelector('a[href="https://"]');
   t.ok(extlink, 'Element ext-link becomes a with href');
+  t.notOk(document.querySelector('a[href="https://nested2"] a'), 'ext-link with xref inside does not produce nested link');
+  t.ok(document.querySelector('a[href="https://nested2"]+span').querySelector('a[data-ams-ref="nested"]'), 'xref inside ext-link moved after ext-link');
 });
 
