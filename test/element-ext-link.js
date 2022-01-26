d54c9effbcb655532b82c1d1e311c52f49472c25
Peter Krautzberger <p.krautzberger@gmail.com>
2022-01-26 10:33:53 +0100

    chore(test/element-ext-link.js): remove console.log

--- test/element-ext-link.js
+++ test/element-ext-link.js
@@ -6,7 +6,6 @@ const tape = require('tape');
 tape('Template: ext-link', async function (t) {
   t.plan(3);
   const document = article;
-  console.log(document.querySelector('a[href="https://nested2"]').parentNode.outerHTML);
   const extlink = document.querySelector('a[href="https://"]');
   t.ok(extlink, 'Element ext-link becomes a with href');
   t.notOk(document.querySelector('a[href="https://nested2"] a'), 'ext-link with xref inside does not produce nested link');
