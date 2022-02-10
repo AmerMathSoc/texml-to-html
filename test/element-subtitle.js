f7eafec889a5a81fe3c8eb230b04ac400aafc744
Peter Krautzberger <p.krautzberger@gmail.com>
2022-02-10 13:11:15 +0100

    feat: support article subtitle
    Presevers article-level subtitle in output.
    * article.js:  recurse title-group (instead of just article-title);
      this allows subtitle.js to kick in.
    * add test case and test
    
    Resolves #351

--- test/element-subtitle.js
+++ test/element-subtitle.js
@@ -1,9 +1,9 @@
 
-const { article } = require('./helper.js');
+const { article, articleAlttitle } = require('./helper.js');
 const tape = require('tape');
 
 tape('subtitle', async function(t) {
-  t.plan(2);
+  t.plan(4);
   const document = article;
 
   const subtitle = document.querySelector('p[data-ams-doc="subtitle"]');
@@ -12,5 +12,13 @@ tape('subtitle', async function(t) {
     subtitle.getAttribute('data-ams-doc-level'),
     subtitle.previousElementSibling.tagName.substring(1),
     'subtitle gets correct data-ams-doc-level'
+  );
+
+  const document2 = articleAlttitle;
+  const articleSubtitle = document2.querySelector('p[data-ams-doc="subtitle"]');
+  t.ok(articleSubtitle, 'article-level subtitle');
+  t.equal(
+    articleSubtitle.previousElementSibling.tagName, 'H1',
+    'article-level subtitle follows title'
   );
 });
