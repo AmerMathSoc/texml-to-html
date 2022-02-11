cb77f1db5959e908514255b81cb54ce407d7dbde
Peter Krautzberger <p.krautzberger@gmail.com>
2022-02-11 14:31:35 +0100

    fix(fn.js): multiple footnotes in ancestor
    Fixes reverse ordering of footnotes within the same "bad ancestor".
    * fn.js: add getElementToInsertAfter()
      * checks if "bad ancestor" is followed by already processed footnotes
      and adds the footnote after those (instead of before)
    * add test case and tests
    
    Fixes #352

--- test/element-fn.js
+++ test/element-fn.js
@@ -4,7 +4,7 @@ const tape = require('tape');
 
 
 tape('Template: fn, fn/label', async function(t) {
-  t.plan(5);
+  t.plan(7);
   const document = article;
   const footnote = document.querySelector('div[role="doc-footnote"]');
   t.ok(footnote, 'fn becomes div with role doc-footnote');
@@ -13,5 +13,11 @@ tape('Template: fn, fn/label', async function(t) {
   t.equal(footnoteLabel.getAttribute('data-ams-doc'), 'label', 'footnote label data attribute');
   t.equal(footnoteLabel.innerHTML, '<sup>Label</sup>', 'footnote label content');
   t.notOk(document.querySelector('#fnid').closest('p'), 'footnote moved out of paragraph')
+  
+  const parWithFootnotes = document.querySelector('#multifootnotes');
+  const fn3 = document.querySelector('div#fnid3[role="doc-footnote"]');
+  const fn4 = document.querySelector('div#fnid4[role="doc-footnote"]');
+  t.equal(parWithFootnotes.nextElementSibling, fn3, 'Paragraph with multiple footnotes followed by first footnote');
+  t.equal(fn3.nextElementSibling, fn4, 'Paragraph with multiple footnotes: first footnote followed by second footnote');
 });
 
