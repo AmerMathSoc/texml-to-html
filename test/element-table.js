241e00676084cd96991f1346f683ba965fb18830
Peter Krautzberger <p.krautzberger@gmail.com>
2022-05-12 12:24:36 +0200

    feat(mapAttributes.js): preserve class attribute
    Preserves class attributes (which is soon used on table elements).
    * mapAttributes: add class to the dictionary, mapping to itself
    * add test case and test.
    
    Resolves #371

--- test/element-table.js
+++ test/element-table.js
@@ -3,11 +3,16 @@ import { article } from './helper.js';
 import tape from 'tape';
 
 tape('table element', async function (t) {
-  t.plan(1);
+  t.plan(2);
   const document = article;
   // NOTE <table> is tested in test/copyElement.js
   t.ok(
     document.querySelector('div[data-ams-doc="table-wrap"] > table'),
     `Table has wrapper`
   );
+  t.ok(
+    document.querySelector('table[class="tbl"]'),
+    `Table has class attributes`
+  );
+
 });
