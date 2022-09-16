bc69a59ef55ffae2690da2c385045e11e69f96db
Peter Krautzberger <p.krautzberger@gmail.com>
2022-09-16 12:45:47 +0200

    feat: support target inside tex-math
    Support new texml markup which uses target instead of \cssId{}{}.
    Rewrites it to \cssId{} but places content in second argument.
    Note. No changes needed after wider roll-out / for #386.
    * fn.js: detach footnote when inside tex-math
      * this prevent checks for closest tex-math from working inside the fn
      (which is desired).
    * formula.js: simplify check to closest tex-math
      * this works given fn.js changes
    * target.js: add support for inside tex-math
    * add test case and test
    
    Part of #382

--- test/element-target.js
+++ test/element-target.js
@@ -4,9 +4,12 @@ import tape from 'tape';
 
 
 tape('Template: target', async function(t) {
-  t.plan(1);
+  t.plan(2);
   const document = article;
   const targetSpan = document.querySelector('section[data-ams-doc="article"] span#target');
   t.ok(targetSpan, 'Convert to span inside article');
+
+  // target in tex-math
+  t.ok([...document.querySelectorAll('[data-ams-doc="math block"]')].find(node => node.innerHTML === '\\cssId{targetMath}{\\tag{$x$}}'), 'Convert to \\cssId inside tex-math');
 });
 
