fd3d321474f9432cf0df4a16d7a65196a4cd4828
Peter Krautzberger <p.krautzberger@gmail.com>
2022-09-22 13:13:00 +0200

    fix: regressions due to table-wrap adoption
    Fixes a regression in label.js due to #382 and clarifies related code.
    * refactor(transformer.js): move table wrap to "fig-like" elements
    * docs(attrib.js): fig/fig-group handling
    * fix(label.js): handle label in table-wrap like fig
    * test: add test case and test for table-wrap caption and label
    
    Fixes #389

--- test/element-table-wrap.js
+++ test/element-table-wrap.js
@@ -3,10 +3,33 @@ import { article } from './helper.js';
 import tape from 'tape';
 
 tape('table-wrap element', async function (t) {
-    t.plan(1);
+    t.plan(6);
     const document = article;
+    const tableWrap = document.querySelector('figure[data-ams-doc="table-wrap"]');
     t.ok(
-        document.querySelector('figure[data-ams-doc="table-wrap"]'),
+        tableWrap,
         `table-wrap element as figure`
+    );
+    const tableCaption = tableWrap.querySelector('figcaption');
+    t.ok(
+        tableCaption,
+        `table caption as figcaption`
+    );
+    t.equal(
+        tableWrap.firstElementChild, tableCaption,
+        `first child of table is table caption `
+    );
+    const tableLabel = tableCaption.querySelector('strong')
+    t.ok(
+        tableLabel,
+        `table label in caption as strong element`
+    );
+    t.equal(
+        tableCaption.firstChild, tableLabel,
+        `first child of table caption is table label`
+    );
+    t.equal(
+        tableLabel.innerHTML, 'Table 1. ',
+        `first child of table caption is table label`
     );
 });
