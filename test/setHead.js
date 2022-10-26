f565d9c5c71554a2fb20781176ed5398d2d657bc
Peter Krautzberger <p.krautzberger@gmail.com>
2022-10-26 14:52:03 +0200

    feat: revise setHead()
    Revises setHead to provide more consistency here and downstream.
    * dom.js: drop lang from template
    * head.js
      * add jsdoc
      * simplify addition of charset meta tag
      * add viewport metatag
      * add DIR attribute (technically not "head" related)
    * update and add tests
    
    Resolves #391

--- test/setHead.js
+++ test/setHead.js
@@ -3,19 +3,13 @@ import { article, articleNometa } from './helper.js';
 import tape from 'tape';
 
 
-tape('setHead()', async function(t) {
-  t.plan(4);
+tape('setHead()', async function (t) {
+  t.plan(5);
   const document = article;
-  const metaCharset = document.head.firstElementChild;
-  t.equal(metaCharset.tagName, 'META', 'first Child of head is a meta tag');
-  t.equal(metaCharset.getAttribute('charset'), 'utf-8', 'first Child of head is meta charset');
-  // TODO switch to modern charset tag and add viewport (once JS lands)
-  // t.equal(metaCharset.tagName, 'META', 'first Child of head is a meta tag');
-  // t.equal(metaCharset.getAttribute('charset'), 'utf-8', 'first Child of head is meta charset with charset="utf-8"');
-  // const viewport = document.head.querySelector('meta[name="viewport"][content="width=device-width"]');
-  // t.ok(viewport);
-  // t.equal(document.title, 'article-title', 'title tag is set');
-  t.equal(document.querySelector('html').getAttribute('lang'), 'en', 'Language attribute on html tag');
+  t.equal(document.head.firstChild, document.head.querySelector('meta[charset="utf-8"]'), 'First child of head is a meta tag');
+  t.ok(document.head.querySelector('meta[content="width=device-width, initial-scale=1"][name="viewport"]'), 'viewport metatag');
+  t.equal(document.documentElement.getAttribute('lang'), 'en', 'Language attribute on html tag');
+  t.equal(document.documentElement.getAttribute('dir'), 'ltr', 'Directionality attribute on html tag');
 
   const document2 = articleNometa;
   t.equal(document2.querySelector('html').getAttribute('lang'), 'fr', 'Language attribute on html tag (not the default)');
