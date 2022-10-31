ae9f47b61c6a45553b6e3ce3dd3ec12f7075b417
Peter Krautzberger <p.krautzberger@gmail.com>
2022-10-31 11:26:39 +0100

    feat: support table-wrap-group
    Adds support for table-wrap-group element.
    * transformer.js: map table-wrap-group to fig.js
    * caption.js: consider table-wrap in table-wrap-group as subfigure
    * label.js: support table-wrap-group like  fig(-group), table-wrap
    * add test case and test
    
    Resolves #393

---
+++ test/element-table-wrap-group.js
@@ -0,0 +1,15 @@
+
+import { article } from './helper.js';
+import tape from 'tape';
+
+tape('table-wrap-group element', async function (t) {
+    t.plan(3);
+    const document = article;
+    const tableWrapGroup = document.querySelector('figure[data-ams-doc="table-wrap-group"]');
+    console.log(tableWrapGroup.outerHTML)
+    t.ok(tableWrapGroup, 'table-wrap element as figure');
+    t.equal(tableWrapGroup.querySelector('figcaption > strong').innerHTML, 'Table with subtables. ', 'table-wrap-group caption');
+    t.equal(
+        tableWrapGroup.querySelector('figure[data-ams-doc="table-wrap"] > figcaption > strong').innerHTML, '(a) ', 'table-wrap label'
+    );
+});
