a162a0407a05c4ffdd6d014821b40e917700b13e
Peter Krautzberger <p.krautzberger@gmail.com>
2022-11-18 10:37:00 +0100

    fix(mapColorAttributes): map border attributes
    Supports new texml output that sets border properties.
    * mapColorAttributes: map border-width and border-style
    * update test cases and test
    
    Fixes #394

--- test/helper-mapColorAttributes.js
+++ test/helper-mapColorAttributes.js
@@ -6,6 +6,6 @@ import tape from 'tape';
 tape('Helper: mapColorAttributes', async function(t) {
   t.plan(1);
   const document = article;
-  t.equal(document.querySelector('span[data-ams-style-color]').getAttribute('data-ams-style-color'), 'color:Teal;background-color:rgb(0.000%, 0.000%, 1.000%);border-color:rgb(0, 140, 10);border-width:medium;', 'color attributes combined as single data-ams-style-color attribute');
+  t.equal(document.querySelector('span[data-ams-style-color]').getAttribute('data-ams-style-color'), 'color:Teal;background-color:rgb(0.000%, 0.000%, 1.000%);border-color:rgb(0, 140, 10);border-width:thin;border-style:solid;', 'color attributes combined as single data-ams-style-color attribute');
 });
 
