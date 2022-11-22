c7fab614ea0d20c7933a083c0a6bead17dbe7653
Peter Krautzberger <p.krautzberger@gmail.com>
2022-11-22 18:36:24 +0100

    chore: remove console.log()

--- test/element-table-wrap-group.js
+++ test/element-table-wrap-group.js
@@ -6,7 +6,6 @@ tape('table-wrap-group element', async function (t) {
     t.plan(3);
     const document = article;
     const tableWrapGroup = document.querySelector('figure[data-ams-doc="table-wrap-group"]');
-    console.log(tableWrapGroup.outerHTML)
     t.ok(tableWrapGroup, 'table-wrap element as figure');
     t.equal(tableWrapGroup.querySelector('figcaption > strong').innerHTML, 'Table with subtables. ', 'table-wrap-group caption');
     t.equal(
