cc218d46565f7ee70096915bfe340665d4ad1b1e
Peter Krautzberger <p.krautzberger@gmail.com>
2022-11-23 13:07:09 +0100

    refactor(pub-date.js): remove extraneous whitespace
    Removes inconsequential whitespace.
    
    Part of #289

--- test/element-pub-date-date.js
+++ test/element-pub-date-date.js
@@ -8,5 +8,5 @@ tape('Template: pub-date, history/date', async function(t) {
   document.querySelectorAll('section[data-ams-doc="copyright-page"] dt').forEach(node => {
     if (node.innerHTML === 'Publication History') pubDate = node.nextElementSibling;
   });
-  t.equal(pubDate.innerHTML.trim(), 'This article was received on <time datetime="1999-09-15">1999-09-15</time>,&nbsp;revised on <time datetime="2000-07-01">2000-07-01</time>, <time datetime="2000-07-02">2000-07-02</time>,  and published on <time datetime="2000-04-13">2000-04-13</time>.', 'pubDate creates content from ../history/date\'s and @iso-8601-date')
+  t.equal(pubDate.innerHTML.trim(), 'This article was received on <time datetime="1999-09-15">1999-09-15</time>,&nbsp;revised on <time datetime="2000-07-01">2000-07-01</time>, <time datetime="2000-07-02">2000-07-02</time>, and published on <time datetime="2000-04-13">2000-04-13</time>.', 'pubDate creates content from ../history/date\'s and @iso-8601-date')
 });
