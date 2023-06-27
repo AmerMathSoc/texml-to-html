b901e6a16625a1a64e462c33361376b858525098
Peter Krautzberger <p.krautzberger@gmail.com>
2023-06-27 10:38:10 +0200

    feat(target.js): heuristics tag extraction
    Heuristically extracts tag data into a new attribute (data-ams-tags).
    * target.js
      * if target starts with "\tag{", we assume it's a single tag
        and extract it into data-ams-tags as stringified JSON array
    * test
      * article.xml: add test case for tag extraction
      * element-target.js: add test for tag extraction
    
    Resolves #401

--- test/element-target.js
+++ test/element-target.js
@@ -4,12 +4,16 @@ import tape from 'tape';
 
 
 tape('Template: target', async function(t) {
-  t.plan(2);
+  t.plan(3);
   const document = article;
   const targetSpan = document.querySelector('section[data-ams-doc="article"] span#target');
   t.ok(targetSpan, 'Convert to span inside article');
 
   // target in tex-math
-  t.ok([...document.querySelectorAll('[data-ams-doc="math block"]')].find(node => node.innerHTML === '\\cssId{targetMath}{_\\tag{$x$}}'), 'Convert to \\cssId inside tex-math');
+  const equationsBlock = [...document.querySelectorAll('[data-ams-doc="math block"]')];
+  t.ok(equationsBlock.find(node => node.innerHTML === '\\cssId{targetMath}{_\\tag{$x$}}'), 'Convert to \\cssId inside tex-math');
+
+  // tag extraction
+  t.equal(equationsBlock.find(node => node.innerHTML === '\\cssId{targetMath2}{\\tag{$x$}}\\cssId{targetMath3}{\\tag{TextTag}}').getAttribute('data-ams-tags'), '["$x$","TextTag"]', 'tag stored as attribute');
 });
 
