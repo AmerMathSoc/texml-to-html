4da69c39d51d15cad31a7becdaf39f59776b5bcc
Peter Krautzberger <p.krautzberger@gmail.com>
2023-09-05 15:12:30 +0200

    fix(xref.js): remove deprecated ref-label handling
    Removes ref-label handling after ref-label was removed from texml
    output.
    * xref.js: stop mapping ref-label  to data-ams-ref-label
    * test
      * article.xml: remove ref-label
      * book.xml: remove ref-label
      * element-xref.js: adjust selector
    Note: not a breaking change this was never used downstream.
    
    Fixes #427

--- test/element-xref.js
+++ test/element-xref.js
@@ -6,7 +6,7 @@ import tape from 'tape';
 tape('Template: xref', async function(t) {
   t.plan(7);
   const document = article;
-  t.ok(document.querySelector('a[href="#rid1"][data-ams-ref="type"][data-ams-ref-label="label"]'), 'xref as anchor with href, data-ams-ref');
+  t.ok(document.querySelector('a[href="#rid1"][data-ams-ref="type"]'), 'xref as anchor with href, data-ams-ref');
   t.ok(document.querySelector('a[href="#rid2"][data-ams-ref="fn"][role="doc-noteref"]'), 'xref with ref-type fn has role doc-noteref');
   t.equal(document.querySelector('a[data-ams-ref="fn"]').firstElementChild.tagName, 'SUP', 'xref with ref-type fn has firstChild sup');
   t.ok(document.querySelector('cite a[href="#rid3"][data-ams-ref="bibr"][role="doc-biblioref"]'), 'xref with ref-type bibr a cite with anchor with href, data-ams-ref, role doc-biblioref');
