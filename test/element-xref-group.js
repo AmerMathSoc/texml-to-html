42aa5e4232504c3578477127dc95cadfb30ecd4f
Peter Krautzberger <p.krautzberger@gmail.com>
2023-09-12 10:27:01 +0200

    feat: support xref-group element
    Adds support for texml's new xref-group element.
    * lib/elements
      * add: xrefgroup.js
        * rewrites to span, adds data attributes
        * for now, no math-mode support
      * x.js: enable pass-through inside xref-group elements
    * lib/transformer.js:  hook up xregroups.js
    * test
      * article.xml: add test case
      * add: element-xref-group.js with snapshot test
    
    Resolves #428

---
+++ test/element-xref-group.js
@@ -0,0 +1,10 @@
+
+import { article } from './helper.js';
+import tape from 'tape';
+
+
+tape('Template: xref-group', async function (t) {
+    t.plan(1);
+    const xrefGroup = article.querySelector('#xrefgroup [data-ams-doc="refgroup"]');
+    t.equal(xrefGroup.outerHTML, '<span data-ams-refrange="xrefgroup1 xrefgroup2 xrefgroup3 xrefgroup4" data-ams-ref="grp" data-ams-doc="refgroup"><a data-ams-ref="grp" href="#xrefgroup1">2</a>–<a data-ams-ref="grp" href="#xrefgroup4">5</a></span>', 'xrefgroup snapshot test')
+});
