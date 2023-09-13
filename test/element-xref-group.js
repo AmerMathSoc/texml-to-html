e6ad0b82141cd884f6cee91f9da68a97b7d9b945
Peter Krautzberger <p.krautzberger@gmail.com>
2023-09-13 10:25:37 +0200

    fix(xref-group): reduce data-ams-refrange value
    Changes the refrange value to only use the middle attribute;
    this helps downstream avoid duplicate treatement of start/end.
    * xref-group.js: change refrage to (just) 'middle'
    * test
      * element-xref-group: adjust snapshot
    
    Part of and closes #431

--- test/element-xref-group.js
+++ test/element-xref-group.js
@@ -6,5 +6,5 @@ import tape from 'tape';
 tape('Template: xref-group', async function (t) {
     t.plan(1);
     const xrefGroup = article.querySelector('#xrefgroup [data-ams-doc="refgroup"]');
-    t.equal(xrefGroup.outerHTML, '<span data-ams-refrange="xrefgroup1 xrefgroup2 xrefgroup3 xrefgroup4" data-ams-ref="grp" data-ams-doc="refgroup"><a data-ams-ref="grp" href="#xrefgroup1">2</a>–<a data-ams-ref="grp" href="#xrefgroup4">5</a></span>', 'xrefgroup snapshot test')
+    t.equal(xrefGroup.outerHTML, '<span data-ams-refrange="xrefgroup2 xrefgroup3" data-ams-ref="grp" data-ams-doc="refgroup"><a data-ams-ref="grp" href="#xrefgroup1">2</a>–<a data-ams-ref="grp" href="#xrefgroup4">5</a></span>', 'xrefgroup snapshot test')
 });
