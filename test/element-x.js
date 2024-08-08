5b77a8642fe78bfd56fb5bfc753d67e9bbd3d719
Peter Krautzberger <p.krautzberger@gmail.com>
2024-08-08 19:23:51 +0200

    feat!(x.js): preserve all x elements as ams-x
    Always preserves x elements and now renames them to
    ams-x.
    - x.js
      - stop throwing out almost all x elements in articles
      - rename x to ams-x and pass through
    - test
      - element-x.js
        - replace all tests with two (check passthrough, check all)
        NOTE: Magic number might come to hurt us.
    
    Resolves #445
    BREAKING CHANGE: all x elements are now preserved as
    ams-x, i.e. downstream will need to remove unwanted ones.

--- test/element-x.js
+++ test/element-x.js
@@ -20,11 +20,8 @@ import tape from 'tape';
 
 
 tape('Template: x', async function(t) {
-  t.plan(4);
+  t.plan(2);
   const document = article;
-  t.equal(document.querySelector('a[href="#rid5"][data-ams-ref="type"]').innerHTML, 'Context ref', 'xref with generic type with x child preserved');
-  t.equal(document.querySelector('cite a[href="#rid4"][data-ams-ref="bibr"][role="doc-biblioref"]').innerHTML, 'ref, note', 'xref with ref-type bibr with x child preserve');
-  t.equal(document.querySelector('p#pwithx').innerHTML, 'Is  ignored.', 'x in article is ignored');
-  const document2 = book;
-  t.equal(document2.querySelector('p#pwithx').innerHTML, 'Is not ignored.', 'x in book is preserved');
+  t.equal(document.querySelector('ams-x').innerHTML, ',', 'x tags renamed to ams-x');
+  t.equal(document.querySelectorAll('ams-x').length, 12, 'All x elements are preserved');
 });
