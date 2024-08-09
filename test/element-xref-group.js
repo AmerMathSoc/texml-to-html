133d8a529446fa47d75f3a02bab0ee42c26aba23
Peter Krautzberger <p.krautzberger@gmail.com>
2024-08-09 13:30:40 +0200

    feat!(xref-groupl.js): preserve in tex-math
    Removes extra handling inside tex-math elements to
    support new texml output.
    - xref-group.js: remove tex-math clause
    - test
      - article.xml: add test case
      - element-xref-group: add snapshot test
    
    Resolves #456
    BREAKING CHANGE: xref-group now preserved in tex-math

--- test/element-xref-group.js
+++ test/element-xref-group.js
@@ -20,7 +20,9 @@ import tape from 'tape';
 
 
 tape('Template: xref-group', async function (t) {
-    t.plan(1);
+    t.plan(2);
     const xrefGroup = article.querySelector('#xrefgroup [data-ams-doc="refgroup"]');
     t.equal(xrefGroup.outerHTML, '<span data-ams-refrange="xrefgroup2 xrefgroup3" data-ams-ref="grp" data-ams-doc="refgroup"><a data-ams-ref="grp" href="#xrefgroup1">2</a>–<a data-ams-ref="grp" href="#xrefgroup4">5</a></span>', 'xrefgroup snapshot test')
+    const xrefGroupMath = article.querySelector('#xrefgroup tex-math [data-ams-doc="refgroup"]');
+    t.equal(xrefGroupMath.outerHTML, '<span data-ams-refrange="xrefgroup2 xrefgroup3" data-ams-ref="grp" data-ams-doc="refgroup"><ams-x>(</ams-x>\\xhref[grp]{#xrefgroup1}{2}<ams-x>)</ams-x>–\\xhref[grp]{#xrefgroup4}{5}</span>', 'xrefgroup in math mode snapshot test')
 });
