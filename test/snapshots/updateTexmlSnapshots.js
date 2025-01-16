22a5c1d32114f93f704e58b96e8fb75498943a9b
Peter Krautzberger <p.krautzberger@gmail.com>
2025-01-16 12:36:17 +0100

    test: improve texml snapshot tests
    Clean up html when refreshing and remove extraneous period from html
    filenames.
    
    - package.json
      - fix refresh scripts to also delete HTML files before refreshing
    - test
      - updateTexmlSnapshots.js
      -  add `.` to extension to remove extraneous `.` from html filename
      - texml-tests
        - refresh HTML files (renamed from `..html` to `.html`)

--- test/snapshots/updateTexmlSnapshots.js
+++ test/snapshots/updateTexmlSnapshots.js
@@ -9,6 +9,6 @@ const files = fs.readdirSync(texmlTestsPath);
 
 files.forEach((file) => {
     if (!file.endsWith('.xml')) return;
-    fs.writeFileSync(path.join(texmlTestsPath, path.basename(file, 'xml') + '.html'), texml2html(fs.readFileSync(path.join(texmlTestsPath, file)).toString()).window.document.toString())
+    fs.writeFileSync(path.join(texmlTestsPath, path.basename(file, '.xml') + '.html'), texml2html(fs.readFileSync(path.join(texmlTestsPath, file)).toString()).window.document.toString())
 });
 
