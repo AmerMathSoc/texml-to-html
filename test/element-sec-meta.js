6f7fe2c718be1db0da41615738e6174a732868fa
Peter Krautzberger <p.krautzberger@gmail.com>
2026-03-16 12:53:43 +0100

    feat!: adjust to texml changes around book-part
    Adjusts to breaking changes in texml, using book-part
    for parts and chapters in books.
    - lib/transformer.js
      - sec-like elements: add book-part
      - passThrough elements: remove book-part, book-part-meta
      - map book-part-meta to SecMeta
    - lib/elements/sec-meta.js
      - change output tagnam from section to div
      - only add data-ams-contributors, data-ams-byline if present
    - tests
      - book--metadata.xml
        - update to new texml markup structure (book-part etc)
      - book.xml
        - update to new texml markup structure (boko-part etc)
        - change sec-meta test case from chapter to section
        - add test case for new book-part-meta markup
      - elemet-sec-meta.js
        - adjust for wrapper tagname change (now div)
        - adjust existing book sec-meta to adjuste test case (now in a section)
        - add tests for book-part-meta test case
      - regenerate HTML
        - article--alttitle.html
        - article.html
        - book-metadata.html
        - book.html
    
    Resolves #495
    BREAKING CHANGE: Changes sec-meta output & expects new texml book markup (#495)

--- test/element-sec-meta.js
+++ test/element-sec-meta.js
@@ -17,27 +17,37 @@
 import { article, articleAlttitle, book } from './helper.js';
 import tape from 'tape';
 
-tape('Template: sec-meta => contrib-group, role, abstract/title', async function (t) {
-  t.plan(10);
+tape('Template: sec-meta', async function (t) {
+  t.plan(16);
 
   const document = book;
   const section = document.querySelector('#secmeta');
-  const secMeta = section.querySelector('section[data-ams-doc="sec-meta"]');
+  const secMeta = section.querySelector('div[data-ams-doc="sec-meta"]');
   t.equal(secMeta.getAttribute('data-ams-contributors'), '{"null":[]}', 'book sec-meta');
-  t.ok(section.querySelector('h1'), 'title becomes h1');
-  t.equal(section.querySelector('h1').innerHTML, '<span data-ams-doc="title">Title</span>', 'title content in heading');
+  t.ok(section.querySelector('h2'), 'title becomes h2');
+  t.equal(section.querySelector('h2').innerHTML, '<span data-ams-doc="title">Title</span>', 'title content in heading');
   t.ok(section.querySelector('[role="doc-abstract"] h2'), 'abstract title becomes h2');
 
+  // book-part-meta
+  const bookPartMeta = document.querySelector('#bookPartMeta>div[data-ams-doc="sec-meta"]');
+  t.equal(bookPartMeta.getAttribute('data-ams-byline'), 'A. Author', 'book-part-meta: byline');
+  console.log()
+  t.equal(bookPartMeta.getAttribute('data-ams-contributors'), '{"authors":[{"name":"A. Author","bio":"","affiliations":["Author University"],"emails":["author@example.com"],"mrauth":"0000","orcid":"0000"}]}', 'book-part-meta: contrib-group');
+  t.ok(bookPartMeta.firstElementChild.tagName, 'h1', 'book-part-meta: title-group');
+  t.ok(bookPartMeta.querySelector('[role="doc-abstract"]'), 'book-part-meta: abstract');
+  t.ok(bookPartMeta.querySelector('ul[data-ams-doc="MSC 2020"]'), 'book-part-meta: MSCs');
+  t.ok(bookPartMeta.querySelector('div[data-ams-doc="funding-group"]'), 'book-part-meta: funding-group');
+
   const document2 = article;
-  const secmeta = document2.querySelector('#secmeta section[data-ams-doc="sec-meta"]');
+  const secmeta = document2.querySelector('#secmeta div[data-ams-doc="sec-meta"]');
   t.ok(secmeta, 'article sec-meta creates section data attribute');
   t.equal(secmeta.getAttribute('data-ams-contributors'), '{"authors":[{"name":"GivenName Surname","bio":"","affiliations":[],"emails":[]}]}', 'article sec-meta');
   t.equal(secmeta.getAttribute('data-ams-byline'), 'GivenName Surname', 'article sec-meta');
-  
+
   const document3 = articleAlttitle;
-  const secmeta3 = document3.querySelector('#secmeta section[data-ams-doc="sec-meta"]');
+  const secmeta3 = document3.querySelector('#secmeta div[data-ams-doc="sec-meta"]');
   t.equal(secmeta3.getAttribute('data-ams-contributors'), '{"authors":[{"name":"Author 1","bio":"Author 1 Bio","affiliations":[],"emails":[]}],"contributors":[{"name":"Author 2","bio":"Author 2 Bio","affiliations":[],"emails":[],"byline":"with"}]}', 'article--alttitle sec-meta');
   t.equal(secmeta3.getAttribute('data-ams-byline'), 'Author 1, with Author 2', 'article sec-meta');
-  
+
   t.ok(secmeta3.querySelector('section[data-ams-doc="notes"]'), 'sec-meta (non-contrib-group) child content');
 });
